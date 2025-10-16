5846968db0bcd256487fef1d2b9d906a5a4dd400
ahmed <m.a.associates295@gmail.com>
2025-10-17 00:20:17 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,9 @@ app.use(cors())
 // env variables
 const port = process.env.port || 6969;
 
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
